824f3d5d4d5c9c28b83a45a0a285fbcfda7da87d
Juanma97 <juanma97@outlook.com>
2018-05-18 14:34:27 +0100

    Third commit

--- run.py
+++ run.py
@@ -2,25 +2,23 @@
 
 import search
 
-ab = search.GPSProblem('S', 'F', search.romania)
+ab = search.GPSProblem('A', 'B', search.romania)
 
 
-#print search.breadth_first_graph_search(ab).path()
-#print search.depth_first_graph_search(ab).path()
+# print search.breadth_first_graph_search(ab).path()
+# print search.depth_first_graph_search(ab).path()
 # print search.iterative_deepening_search(ab).path()
 # print search.depth_limited_search(ab).path()
-print """----------------------------------------------"""
 
-# Ramificacion y acotacion
 print """[Ramificacion y acotacion: ]"""
 print search.branchAndBound(ab).path()
 
 print """----------------------------------------------"""
-# Ramificacion y acotacion con subestimacion
+
 print """[Ramificacion y acotacion con subestimacion: ]"""
 print search.branchAndBoundSubestimation(ab).path()
 
-#print search.astar_search(ab).path()
+# print search.astar_search(ab).path()
 
 # Result:
 # [<Node B>, <Node P>, <Node R>, <Node S>, <Node A>] : 101 + 97 + 80 + 140 = 418
